61c9cf7685bb8689f02ae0abf349d6520c1cd4c4
Jacob Green <jacob.green1@gmail.com>
2026-09-02 21:48:57 -0500

    Need to continue the tutorials.

--- ingest.py
+++ ingest.py
@@ -16,7 +16,29 @@ GEMINI_PROJECT_NAME=os.environ["GEMINI_PROJECT_NUMBER"]
 GEMINI_PROJECT_NUMBER=os.environ["GEMINI_PROJECT_NUMBER"]
 LANGSMITH_API_KEY=os.environ["LANGSMITH_API_KEY"]
 
-print(GEMINI_API_KEY)
-print(GEMINI_PROJECT_NUMBER)
-print(GEMINI_PROJECT_NAME)
-print(LANGSMITH_API_KEY)
+#print(GEMINI_API_KEY)
+#print(GEMINI_PROJECT_NUMBER)
+#print(GEMINI_PROJECT_NAME)
+#print(LANGSMITH_API_KEY)
+
+def load_documents(docs_dir: str = "./docs"):
+    """Load all supported documents from a directory."""
+    loaders = {
+            ".txt": TextLoader,
+            ".pdf": PyPDFLoader,
+            }
+    documents = []
+    for file in os.listdir(docs_dir):
+        ext = os.path.splitext(file)[1].lower()
+        if ext in loaders:
+            loader = loaders[ext](os.path.join(docs_dir, file))
+            documents.extend(loader.load())
+    print(f"Loaded {len(documents)} documents")
+    return documents
+
+def split_documents(documents,)
+
+
+if __name__ == "__main__":
+    docs = load_documents()
+    print(docs)
